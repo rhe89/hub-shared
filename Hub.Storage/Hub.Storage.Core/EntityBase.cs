c9ca8ef266b88e5c904c113dfefe417fb1e12d2d
Roar <roar.h.eriksen@gmail.com>
2020-11-13 23:50:50 +0100

    Added core-project for Hub.Storage

--- Hub.Storage/Hub.Storage.Core/EntityBase.cs
+++ Hub.Storage/Hub.Storage.Core/EntityBase.cs
@@ -2,7 +2,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Hub.Storage.Entities
+namespace Hub.Storage.Core
 {
     public abstract class EntityBase
     {
