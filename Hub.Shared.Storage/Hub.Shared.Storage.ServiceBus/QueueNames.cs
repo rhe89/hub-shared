1f69a074613de8492b4c3d54ab89dfa6e2362954
Roar <roar.h.eriksen@gmail.com>
2022-04-18 21:40:46 +0200

    Added constants for queue names

---
+++ Hub.Shared.Storage/Hub.Shared.Storage.ServiceBus/QueueNames.cs
@@ -0,0 +1,25 @@
+namespace Hub.Shared.Storage.ServiceBus;
+
+public static class QueueNames
+{
+    //Crypto
+    public static readonly string UpdateCoinbaseProAccounts = "updatecoinbaseproaccounts";
+    public static readonly string UpdateCoinbaseProAssetHistory = "updatecoinbaseproassethistory";
+        
+    public static readonly string UpdateCoinbaseAccounts = "updatecoinbaseaccounts";
+    public static readonly string UpdateCoinbaseExchangeRates = "updatecoinbaseexchangerates";
+    public static readonly string UpdateCoinbaseAssetHistory = "updatecoinbaseassethistory";
+        
+    public static readonly string CryptoAssetHistoryUpdated = "cryptoassethistoryupdated";
+    public static readonly string CryptoAccountsUpdated = "cryptoaccountsupdated";
+    public static readonly string ExchangeRatesUpdated = "exchangeratesupdated";
+    
+    //Banking
+    public static readonly string UpdateBankingAccounts = "updatebankingaccounts";
+    public static readonly string UpdateBankingTransactions = "updatebankingtransactions";
+    public static readonly string UpdateBankingAccountBalances = "updatebankingaccountbalances";
+    
+    public static readonly string BankingAccountsUpdated = "bankingaccountsupdated";
+    public static readonly string BankingTransactionsUpdated = "bankingtransactionsupdated";
+    public static readonly string BankingAccountBalanceHistoryUpdated = "bankingaccountbalancehistoryupdated";
+}
