980436681f48f1bb8fb41fb318da58d893dad5ca
Roar <roar.h.eriksen@gmail.com>
2022-04-18 21:40:29 +0200

    Added constants for api-endpoints

---
+++ Hub.Shared.Settings/ApiEndpoints.cs
@@ -0,0 +1,8 @@
+namespace Hub.Shared.Settings;
+
+public static class ApiEndpoints
+{
+    public static readonly string Crypto = "CRYPTO_API_HOST";
+    public static readonly string Banking = "BANKING_API_HOST";
+    public static readonly string Spreadsheet = "SPREADSHEET_API_HOST";
+}
