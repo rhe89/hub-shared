15199456d9626d30b51e57bfe7cd67fe3c724019
Roar <roar.h.eriksen@gmail.com>
2022-05-10 21:59:20 +0200

    Remove unused method

--- Hub.Shared.Storage/Hub.Shared.Storage.Repository/ServiceCollectionExtensions.cs
+++ Hub.Shared.Storage/Hub.Shared.Storage.Repository/ServiceCollectionExtensions.cs
@@ -11,24 +11,6 @@ namespace Hub.Shared.Storage.Repository;
 public static class ServiceCollectionExtensions
 {
     [UsedImplicitly]
-    public static void AddDatabase<TDbContext>(this IServiceCollection serviceCollection, IConfiguration configuration, string connectionStringKey, string migrationAssembly)
-        where TDbContext : HubDbContext
-    {
-        serviceCollection.AddDbContext<TDbContext>(options => 
-            options.UseSqlServer(configuration.GetValue<string>(connectionStringKey), 
-                x => x.MigrationsAssembly(migrationAssembly)));
-            
-        serviceCollection.TryAddTransient<IHubDbRepository, HubDbRepository<TDbContext>>();
-        serviceCollection.TryAddTransient<ICommandConfigurationProvider, CommandConfigurationProvider>();
-        serviceCollection.TryAddTransient<ICommandConfigurationFactory, CommandConfigurationFactory>();
-            
-        serviceCollection.AddAutoMapper(c =>
-        {
-            c.AddProfile<CommandConfigurationProfile>();
-        });
-    }
-        
-    [UsedImplicitly]
     public static void AddDatabase<TDbContext>(this IServiceCollection serviceCollection, IConfiguration configuration, string connectionStringKey)
         where TDbContext : HubDbContext
     {
