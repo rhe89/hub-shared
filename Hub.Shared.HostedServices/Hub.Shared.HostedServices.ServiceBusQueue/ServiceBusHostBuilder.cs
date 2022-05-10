f994530a4e4edb37d6e50738ca2541d010284064
Roar <roar.h.eriksen@gmail.com>
2022-05-10 21:56:15 +0200

    Use new Hub.Shared.AppConfiguration-package for building host

---
+++ Hub.Shared.HostedServices/Hub.Shared.HostedServices.ServiceBusQueue/ServiceBusHostBuilder.cs
@@ -0,0 +1,31 @@
+using System;
+using Hub.Shared.Configuration;
+using Hub.Shared.Storage.Repository;
+using Hub.Shared.Storage.ServiceBus;
+using JetBrains.Annotations;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace Hub.Shared.HostedServices.ServiceBusQueue;
+
+[UsedImplicitly]
+public static class ServiceBusHostBuilder
+{
+    [UsedImplicitly]
+    public static IHostBuilder CreateHostBuilder<TDbContext>(string [] args, 
+        string connectionStringKey)
+        where TDbContext : HubDbContext
+    {
+        return Host.CreateDefaultBuilder(args)
+            .GetDefaultHostBuilder()
+            .ConfigureServices((hostBuilderContext, serviceCollection) =>
+            {
+                serviceCollection.AddDatabase<TDbContext>(hostBuilderContext.Configuration, connectionStringKey);
+                serviceCollection.TryAddTransient<IQueueProcessor, QueueProcessor>();
+                serviceCollection.AddApplicationInsightsTelemetryWorkerService();
+                serviceCollection.AddSingleton<ITelemetryInitializer>(new CloudRoleNameInitializer(Environment.GetEnvironmentVariable("CLOUD_ROLE_NAME")));
+            });
+    }
+}
