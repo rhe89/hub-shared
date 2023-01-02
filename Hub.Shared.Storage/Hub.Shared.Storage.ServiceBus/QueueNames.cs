5c0365aab990de6d4e4932b2ea5b2c1d4708a81d
Roar <roar.h.eriksen@gmail.com>
2023-01-02 18:50:24 +0100

    Update package version

--- Hub.Shared.Storage/Hub.Shared.Storage.ServiceBus/QueueNames.cs
+++ Hub.Shared.Storage/Hub.Shared.Storage.ServiceBus/QueueNames.cs
@@ -3,23 +3,13 @@ namespace Hub.Shared.Storage.ServiceBus;
 public static class QueueNames
 {
     //Crypto
-    public static readonly string UpdateCoinbaseProAccounts = "updatecoinbaseproaccounts";
-    public static readonly string UpdateCoinbaseProAssetHistory = "updatecoinbaseproassethistory";
-
-    public static readonly string UpdateCoinbaseAccounts = "updatecoinbaseaccounts";
-    public static readonly string UpdateCoinbaseExchangeRates = "updatecoinbaseexchangerates";
-    public static readonly string UpdateCoinbaseAssetHistory = "updatecoinbaseassethistory";
-
-    public static readonly string CryptoAssetHistoryUpdated = "cryptoassethistoryupdated";
-    public static readonly string CryptoAccountsUpdated = "cryptoaccountsupdated";
-    public static readonly string ExchangeRatesUpdated = "exchangeratesupdated";
+    public static readonly string UpdateCurrencyPrices = "updatecurrencyprices";
 
     //Banking
     public static readonly string UpdateScheduledTransactions = "updatescheduledtransactions";
     public static readonly string UpdateAccountBalancesForNewMonth = "updateaccountbalancesfornewmonth";
     public static readonly string UpdateSbankenTransactions = "updatesbankentransactions";
     public static readonly string UpdateBulderBankTransactions = "updatebulderbanktransactions";
-    public static readonly string CategorizeTransactions = "categorizetransactions";
     public static readonly string CalculateCreditCardPayments = "calculatecreditcardpayments";
     public static readonly string CalculateMonthlyBudget = "calculatemonthlybudgetresult";
 
